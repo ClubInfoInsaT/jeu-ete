6b273b775898195ba154bb8ef3eb878248154aca
scanim-valentin <valentin.guerbette@hotmail.fr>
2022-08-26 08:05:26 +0200

    correction position pivot chunks

--- Assets/Scripts/Generation.cs
+++ Assets/Scripts/Generation.cs
@@ -8,7 +8,6 @@ public class Generation : MonoBehaviour
 
 	private ChunkTemplate templates;
     private Transform gridTrans;
-    private Vector3 newPos;
     private GameObject newChunk; 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +20,22 @@ public class Generation : MonoBehaviour
 
     void Spawn()
     {
-        newPos = transform.position; 
+        float newPos = 0f;
+        newChunk = templates.chunks[Random.Range(0, templates.chunks.Length)];
         for (int i = 0; i < 10; i++)
         {
-            Debug.Log(newPos);
+            Debug.Log("["+ newChunk.transform.name + "]"+"New Position : "+new Vector3(newPos, 0, 0));
+            Instantiate(newChunk, new Vector3(newPos,0,0) , Quaternion.identity, gridTrans);
+            Debug.Log("[" + newChunk.transform.name + "]" + " size :" + newChunk.transform.GetChild(0).GetComponent<UnityEngine.Tilemaps.Tilemap>().size.x);
+            newPos += newChunk.transform.GetChild(0).GetComponent<UnityEngine.Tilemaps.Tilemap>().size.x;
             newChunk = templates.chunks[Random.Range(0, templates.chunks.Length)];
-            Debug.Log(newChunk.transform.GetComponent<UnityEngine.Tilemaps.Tilemap>().size);
-            newPos += new Vector3(newChunk.transform.GetComponent<UnityEngine.Tilemaps.Tilemap>().size.x / 2, 0, 0);
-            Instantiate(newChunk, newPos, Quaternion.identity, gridTrans);
-            newPos += new Vector3(newChunk.transform.GetComponent<UnityEngine.Tilemaps.Tilemap>().size.x / 2, 0,0); 
+            
+
+            //newChunk = templates.chunks[Random.Range(0, templates.chunks.Length)];
+
+            //newPos += new Vector3(newChunk.transform.GetComponent<UnityEngine.Tilemaps.Tilemap>().size.x / 2, 0, 0);
+            //Instantiate(newChunk, newPos, Quaternion.identity, gridTrans);
+            //newPos += new Vector3(newChunk.transform.GetComponent<UnityEngine.Tilemaps.Tilemap>().size.x / 2, 0,0); 
         }
     }
 }
