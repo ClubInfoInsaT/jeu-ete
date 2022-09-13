4553d2b82e50637bcff844ace55b948555f43f61
Newg <92101871+Newglear@users.noreply.github.com>
2022-09-13 08:52:11 +0200

    Ajout des collectibles

---
+++ Assets/Scripts/Collectible_Manager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Collectible_Manager : MonoBehaviour
+{
+    public SpriteRenderer SpriteRenderer; 
+    public Sprite[] sprites;
+
+    private void Start()
+    {
+        int index = Random.Range(0,sprites.Length);
+        SpriteRenderer.sprite = sprites[index];
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ScoreManager.score++;
+            Destroy(gameObject);
+        }
+    }
+}
